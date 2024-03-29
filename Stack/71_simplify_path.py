6b1e2113771c7d455e5c8643f9b4effc7190bbe8
chw2006 <chw2006@gmail.com>
2024-03-29 01:45:44 -0500

    Update 71_simplify_path.py

--- Stack/71_simplify_path.py
+++ Stack/71_simplify_path.py
@@ -33,3 +33,36 @@ class Solution:
 # T: O(N). Splitting takes O(N). Going through the directories is O(K) if we have k directories.
 # Going through the stack is O(K).
 # S: O(N). We keep a stack and a directories array. 
+    
+# Meta asks this problem with a variation. They give you 2 inputs, one for the current working directory and another for the destination path. 
+def metaSimplifyPath(cwd, destination):
+    # This should be the very similar to the original problem
+    # One way to solve it would be to join the cwd and destination strings.
+    # It depends on how they format the inputs. If they are required to have a '/' at the start of each path, then we do not need to join with a '/'.
+    # Although technically it should not matter. 
+    stack = []
+    result = ""
+    combined = cwd + destination
+    directories = combined.split('/')
+
+    # Go through each directory, if we see a blank, ignore it. If we see a ., ignore it. If we see a .., pop from stack.
+    # If we see a directory, add it to stack. 
+    for dir in directories:
+        if dir == '' or dir == '.':
+            continue
+        elif dir == '..':
+            stack.pop()
+        # Case of a real directory
+        else:
+            stack.append(dir)
+    
+    # Print the path. Since it needs to start with a '/', add it to every directory
+    if stack:
+        for dir in stack:
+            result += "/" + dir
+    else:
+        result = "/"
+    
+    return result
+
+print(metaSimplifyPath("/home/foo/", "/x/y/.."))
