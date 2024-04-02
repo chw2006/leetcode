a107b79a02d08fb483e73f4ce66aaae2c4573387
chw2006 <chw2006@gmail.com>
2024-04-01 20:47:25 -0500

    Update 1868_product_of_2_run_length_encoded_arrays.py

--- 2 Pointers/1868_product_of_2_run_length_encoded_arrays.py
+++ 2 Pointers/1868_product_of_2_run_length_encoded_arrays.py
@@ -6,47 +6,50 @@ def findRLEArray(encoded1, encoded2):
     # If the frequencies are not the same, take the min of the 2.
     # Increment the pointer of the smaller frequency. 
     # Set the frequency of the longer one to longer - shorter. 
-    
-    i = 0
-    j = 0
+    p1 = 0
+    p2 = 0
     res = []
-    # Finds the index of the array with the input product
-    def find_product(product):
-        for i in range(len(res)):
-            if res[i][0] == product:
-                return i
-        return -1
 
-    while i < len(encoded1) and j < len(encoded2):
-        product = encoded1[i][0] * encoded2[j][0]
-        # If this value already exists in the result, add to it, do not put in a new entry
-        product_index = find_product(product)
-        # If the product is in the result, add ot it
-        if product_index >= 0:
-            # We are adding to the frequency of the product index we found
-            res[product_index][1] += min(encoded1[i][1], encoded2[j][1])
+    while p1 < len(encoded1) and p2 < len(encoded2):
+        # Get the frequencies and products of the 2 arrays
+        freq1 = encoded1[p1][1]
+        freq2 = encoded2[p2][1]
+        freq = min(freq1, freq2)
+        product = encoded1[p1][0] * encoded2[p2][0]
+        # Check if product exists as last val in res
+        if res and product == res[-1][0]:
+            # If so, update freq
+            res[-1][1] += freq
         else:
-            # Otherwise, just add it to the result
-            res.append([product, min(encoded1[i][1], encoded2[j][1])])
-
-        # Increment both if the freq is equal
-        if encoded1[i][1] == encoded2[j][1]:
-            i += 1
-            j += 1
-        # Increment the pointer of the smaller frequency and update the frequency of the larger
-        elif encoded1[i][1] < encoded2[j][1]:
-            encoded2[j][1] -= encoded1[i][1]
-            i += 1
+            # Otherwise, add product, freq to result
+            res.append([product, freq])
+        # Move the pointers based on freq1 and freq2
+        # If freq are equal, move both pointers.
+        if freq1 == freq2:
+            p1 += 1
+            p2 += 1
+        # Increment the smaller pointer (it's been used)
+        # Decrement smaller from larger freq
+        elif freq1 > freq2:
+            #Increment smaller
+            p2 += 1
+            # Update freq of larger
+            encoded1[p1][1] -= freq2
         else:
-            encoded1[i][1] -= encoded2[j][1]
-            j += 1
-        
+            p1 +=1
+            encoded2[p2][1] -= freq1
+    
     return res
 
 encoded1 = [[1,3],[2,3]]
 encoded2 = [[6,3],[3,3]]
 print(findRLEArray(encoded1, encoded2))
+encoded1 = [[1, 3], [2, 1], [3, 2]]
+encoded2 = [[2, 3], [3, 3]]
+print(findRLEArray(encoded1, encoded2))
 
+# T: O(N) - go through both arrays once.
+# S: O(N) - we have to keep the result
 
             
             
