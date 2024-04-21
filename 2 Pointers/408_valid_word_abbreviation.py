40e92627d15093f1135fd7fb73db3d3663edfaeb
chw2006 <chw2006@gmail.com>
2024-04-20 21:43:21 -0500

    Update 408_valid_word_abbreviation.py

--- 2 Pointers/408_valid_word_abbreviation.py
+++ 2 Pointers/408_valid_word_abbreviation.py
@@ -5,41 +5,44 @@
 # If both characters are letters, we can compare them straight up. 
 # If they do not match, we can return False
 
-def valid_word_abbreviation(s, abbr):
-    p1 = 0
-    p2 = 0
-    while p1 < len(s) and p2 < len(abbr):
-        # Case for when abbr is a digit
-        if abbr[p2].isdigit():
-            digit = 0
-            # The digit can be multiple characters, turn that into an integer
-            while p2 < len(abbr) and abbr[p2].isdigit():
-                # If we have a digit that is multipe characters, calculate its value
-                # by using prev * 10 + digit
-                digit = (digit * 10) + int(abbr[p2])
-                # Increment p2
-                p2 += 1
-                if digit > len(s) - p1:
-                    return False
-            # Increment p1 depending on the value of the digit
-            p1 += digit
-        else:
-            # For the case where both characters are letters
-            # If they aren't equal, we can return False
-            if s[p1] != abbr[p2]:
-                return False
-            # Increment both pointers
-            p1 += 1
-            p2 += 1
-    # If we get here, we can return True
-    return True
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        # If both chars are alpha, then we can compare.
+        # If the abbr is a number, parse the number and then increment the word pointer by that number
+        # An abbr number must not start with 0. 
+        # We must make sure both strings are exhausted in order to return true
 
-s = 'internationalization'
-abbr = 'i18n'
-print(valid_word_abbreviation(s, abbr))  # True
-s = 'apple'
-abbr = 'a2e'
-print(valid_word_abbreviation(s, abbr))  # False'
+        p1 = 0
+        p2 = 0
+
+        if len(abbr) > len(word):
+            return False
+
+        while p1 < len(word) and p2 < len(abbr):
+            # If abbreviation is alpha, we can compare
+            if abbr[p2].isalpha():
+                if word[p1] != abbr[p2]:
+                    return False
+                p1 += 1
+                p2 += 1
+            # If it is a number, we must find its number and fast forward p1 by it. 
+            else:
+                # If abbr is a number
+                num = 0
+                # Look for leading 0s
+                if abbr[p2] == '0':
+                    return False
+                while p2 < len(abbr) and abbr[p2].isdigit():
+                    num = num * 10 + int(abbr[p2])
+                    p2 += 1
+                # Increment p1 by num
+                p1 += num
+                
+        # Both pointers must be at the end of each word in order to return True
+        if p1 == len(word) and p2 == len(abbr):
+            return True
+        else:
+            return False
 
 #T: O(N) we make one pass through both strings.
 #S: O(1) we only keep track of pointer values. 
